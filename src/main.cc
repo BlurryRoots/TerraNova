adc4189dfcbae7ab20ba48342cffd91feb402a4d
blurryroots <blurryroots@posteo.de>
2015-05-04 18:57:34 +0200

    basic layout

--- src/main.cc
+++ src/main.cc
@@ -7,194 +7,133 @@
 #include <iostream>
 #include <string>
 
-class IApplication {
-public:
-	virtual ~IApplication ()
-	{}
+struct IGame {
+	virtual ~IGame () {}
 
-	virtual void on_mousebutton_down (SDL_Event event) = 0;
-	virtual void on_mousebutton_up (SDL_Event event) = 0;
-
-	virtual void on_key_up (SDL_Event event) = 0;
-	virtual void on_key_down (SDL_Event event) = 0;
-
-	virtual void on_window_resize (SDL_Event event) = 0;
-
-	virtual void on_update (Uint32 dt) = 0;
-
+	virtual void on_update (float dt) = 0;
 	virtual void on_render () = 0;
+
+	virtual bool has_closing_request () = 0;
 };
 
-class TestApp : IApplication {
+class TestGame
+: public IGame {
 
 public:
-	void on_mousebutton_down (SDL_Event event) {
-		log ("mouse down");
-	}
-
-	void on_mousebutton_up (SDL_Event event) {
-		log ("mouse up");
-	}
-
-	void on_key_up (SDL_Event event) {
-		log ("key up");
-	}
-
-	void on_key_down (SDL_Event event) {
-		log ("key down");
-	}
-
-	void on_window_resize (SDL_Event event) {
-		log ("window resize");
-
-		int w = event.window.data1;
-		int h = event.window.data2;
-		float aspectAdjust;
-
-		aspectAdjust = (4.0f / 3.0f) / ((float)w / h);
-		glViewport(0, 0, w, h);
-		glMatrixMode(GL_PROJECTION);
-		glLoadIdentity();
-		glOrthof(-2.0, 2.0, -2.0 * aspectAdjust, 2.0 * aspectAdjust, -20.0, 20.0);
-		glMatrixMode(GL_MODELVIEW);
-		glLoadIdentity();
-		glEnable(GL_DEPTH_TEST);
-		glDepthFunc(GL_LESS);
-		glShadeModel(GL_SMOOTH);
-	}
-
-	void on_update (Uint32 dt) {
-
+	void on_update (float dt) {
+		this->closing_request = true;
 	}
 
 	void on_render () {
-		glClearColor (0.0, 0.0, 0.0, 1.0);
-		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		std::cout << "Here we go!" << std::endl;
 	}
 
-	void log (std::string msg) {
-		std::cout << msg << std::endl;
+	bool has_closing_request () {
+		return this->closing_request;
 	}
+
+private:
+	bool closing_request;
+
 };
 
-/*
-if (event.key.keysym.scancode == SDL_SCANCODE_AC_BACK)
-{
-	done = 1;
-}
-*/
+class SDL_Application {
+public:
+	SDL_Application () {
+		this->sdl_status = SDL_Init (SDL_INIT_EVERYTHING);
+		if (0 > this->sdl_status) {
+			SDL_Log ("Unable to initialize SDL");
+		}
+		else {
+			SDL_GetDesktopDisplayMode (0, &(this->mode));
 
-int
-initialize_sdl (SDL_GLContext *context, SDL_Window *window) {
-	SDL_DisplayMode mode;
+			SDL_GL_SetAttribute (SDL_GL_BUFFER_SIZE, 16);
+			SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 1);
 
-	if (0 > SDL_Init (SDL_INIT_EVERYTHING)) {
-		SDL_Log("Unable to initialize SDL");
+			// Create our window centered
+			this->window = SDL_CreateWindow ("GLES example",
+				SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+				//mode.w, mode.h,
+				800, 600,
+				SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
+			);
 
-		return 1;
+			// Create our opengl context and attach it to our window
+			this->context = SDL_GL_CreateContext (this->window);
+
+			SDL_GL_MakeCurrent (this->window, this->context);
+
+			SDL_GL_SetSwapInterval (1);
+		}
+
+		// Extension wrangler initialising
+		glewExperimental = GL_TRUE;
+		this->glew_status = glewInit ();
 	}
 
-	SDL_GetDesktopDisplayMode (0, &mode);
+	void run (IGame &game) {
 
-	SDL_GL_SetAttribute (SDL_GL_BUFFER_SIZE, 16);
-	SDL_GL_SetAttribute (SDL_GL_CONTEXT_MAJOR_VERSION, 1);
+		if (GLEW_OK != this->glew_status) {
+			std::cout << "Could not initialize glew!" << std::endl;
+			return;
+		}
 
-	// Create our window centered
-	window = SDL_CreateWindow ("GLES example",
-		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
-		mode.w, mode.h,
-		SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN
-	);
+		SDL_Event e;
+		bool keep_running = true;
+		while (keep_running
+			&& ! game.has_closing_request ()) {
+			// update game logic
+			game.on_update (0);
 
-	// Create our opengl context and attach it to our window
-	*context = SDL_GL_CreateContext (window);
+			//e is an SDL_Event variable we've declared before entering the main loop
+			while (SDL_PollEvent (&e)) {
+				switch (e.type) {
+					case SDL_QUIT:
+						keep_running = false;
+						break;
+					case SDL_MOUSEBUTTONUP:
+						keep_running = false;
+				}
+			}
 
-	SDL_GL_MakeCurrent (window, *context);
+			//
+			game.on_render ();
+			SDL_GL_SwapWindow (window);
+		}
+	}
 
-	SDL_GL_SetSwapInterval (1);
+	~SDL_Application () {
+		if (0 < this->sdl_status) {
+			SDL_GL_DeleteContext (context);
+			SDL_DestroyWindow (window);
+		}
 
-	return 0;
-}
+		SDL_Quit ();
+	}
 
-void
-shutdown_sdl (SDL_GLContext context, SDL_Window *window) {
-	SDL_GL_DeleteContext (context);
-	SDL_DestroyWindow (window);
-	SDL_Quit ();
-}
+private:
+	SDL_Window *window;
+	SDL_GLContext context;
+	SDL_DisplayMode mode;
+
+	int sdl_status;
+	int glew_status;
+
+};
 
 int
 main (int argc, char **argv) {
-
-	SDL_Window *window = nullptr;
-	SDL_GLContext context;
-
-	if (! initialize_sdl (&context, window)) {
-		return 1;
+	SDL_Application application;
+	try {
+		auto game = TestGame ();
+		application.run (game);
 	}
-
-	// Extension wrangler initialising
-	glewExperimental = GL_TRUE;
-	GLuint glew_status = glewInit ();
-	if (GLEW_OK != glew_status) {
-		std::cout << "Could not initialize glew!" << std::endl;
-
-		shutdown_sdl (context, window);
-		return 1;
+	catch (std::exception &ex) {
+		std::cout << "Cought: " << ex.what () << std::endl;
 	}
-
-	TestApp app;
-
-	SDL_Event event;
-	Uint32 then = SDL_GetTicks ();
-	Uint32 now = 0;
-	Uint32 frames = 0;
-	int done = 0;
-
-	while (! done) {
-		app.on_update (SDL_GetTicks ());
-
-		++frames;
-		while (SDL_PollEvent (&event)) {
-			switch (event.type) {
-				case SDL_KEYDOWN:
-					app.on_key_down (event);
-					break;
-
-				case SDL_MOUSEBUTTONDOWN:
-					app.on_mousebutton_down (event);
-					break;
-
-				case SDL_MOUSEBUTTONUP:
-					app.on_mousebutton_up (event);
-					break;
-
-				case SDL_WINDOWEVENT:
-					switch (event.window.event) {
-						case SDL_WINDOWEVENT_RESIZED:
-							SDL_GL_SwapWindow (window);
-							app.on_window_resize (event);
-							break;
-					}
-					break;
-			}
-
-		}
-
-		app.on_render ();
-		SDL_GL_SwapWindow (window);
+	catch (...) {
+		std::cout << "Cought unkown exception :(" << std::endl;
 	}
-
-	/* Print out some timing information */
-	now = SDL_GetTicks();
-	if (now > then)
-	{
-		SDL_Log ("%2.2f frames per second\n",
-			((double)frames * 1000) / (now - then)
-		);
-	}
-
-	shutdown_sdl (context, window);
 
 	return 0;
 }
