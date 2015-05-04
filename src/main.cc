d82eeeb2e428e65261029d077085bff9650553bf
blurryroots <blurryroots@posteo.de>
2015-05-04 19:16:18 +0200

    more layout stuff

--- src/main.cc
+++ src/main.cc
@@ -10,26 +10,57 @@
 struct IGame {
 	virtual ~IGame () {}
 
+	virtual void on_initialize () = 0;
+
 	virtual void on_update (float dt) = 0;
 	virtual void on_render () = 0;
 
+	virtual void on_shutdown_request () = 0;
 	virtual bool has_closing_request () = 0;
+	virtual void on_shutdown () = 0;
 };
 
 class TestGame
 : public IGame {
 
 public:
-	void on_update (float dt) {
+	void on_initialize () {
+		glEnable (GL_BLEND);
+		glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		glEnable (GL_DEPTH_TEST);
+		glDepthFunc (GL_LESS);
+
+		glEnable (GL_CULL_FACE);
+		glCullFace (GL_BACK);
+		glFrontFace (GL_CCW);
+
+		log ("Starting...");
+	}
+
+	void on_shutdown_request () {
 		this->closing_request = true;
 	}
 
+	void on_update (float dt) {
+
+	}
+
 	void on_render () {
-		std::cout << "Here we go!" << std::endl;
+		glClearColor (0.0f, 0.0f, 0.0f, 1.0f);
+		glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	}
+
+	void on_shutdown () {
+
 	}
 
 	bool has_closing_request () {
 		return this->closing_request;
+	}
+
+	void log (std::string msg) {
+		std::cout << msg << std::endl;
 	}
 
 private:
@@ -72,34 +103,41 @@ public:
 	}
 
 	void run (IGame &game) {
-
 		if (GLEW_OK != this->glew_status) {
 			std::cout << "Could not initialize glew!" << std::endl;
 			return;
 		}
 
-		SDL_Event e;
-		bool keep_running = true;
-		while (keep_running
-			&& ! game.has_closing_request ()) {
-			// update game logic
-			game.on_update (0);
+		try {
+			game.on_initialize ();
 
-			//e is an SDL_Event variable we've declared before entering the main loop
-			while (SDL_PollEvent (&e)) {
-				switch (e.type) {
-					case SDL_QUIT:
-						keep_running = false;
-						break;
-					case SDL_MOUSEBUTTONUP:
-						keep_running = false;
+			SDL_Event e;
+			bool keep_running = true;
+			while (keep_running
+				&& ! game.has_closing_request ()) {
+				game.on_update (0);
+
+				//e is an SDL_Event variable we've declared before entering the main loop
+				while (SDL_PollEvent (&e)) {
+					switch (e.type) {
+						case SDL_QUIT:
+							game.on_shutdown_request ();
+							break;
+					}
 				}
-			}
 
-			//
-			game.on_render ();
-			SDL_GL_SwapWindow (window);
+				game.on_render ();
+				SDL_GL_SwapWindow (window);
+			}
 		}
+		catch (std::exception &ex) {
+			std::cout << "Cought: " << ex.what () << std::endl;
+		}
+		catch (...) {
+			std::cout << "Cought unkown exception :(" << std::endl;
+		}
+
+		game.on_shutdown ();
 	}
 
 	~SDL_Application () {
@@ -123,17 +161,10 @@ private:
 
 int
 main (int argc, char **argv) {
+	TestGame game;
+
 	SDL_Application application;
-	try {
-		auto game = TestGame ();
-		application.run (game);
-	}
-	catch (std::exception &ex) {
-		std::cout << "Cought: " << ex.what () << std::endl;
-	}
-	catch (...) {
-		std::cout << "Cought unkown exception :(" << std::endl;
-	}
+	application.run (game);
 
 	return 0;
 }
